6b4b0698482ea3e1094fed36ff092b56b69a17f5
grnch <grnch.net@gmail.com>
2017-08-04 08:00:47 +0300

    Creating an element by click

--- js/main.js
+++ js/main.js
@@ -5,4 +5,30 @@ var renderer = PIXI.autoDetectRenderer();
 document.getElementById('work-area').appendChild(renderer.view);
 
 var stage = new PIXI.Container();
-var Graphics = PIXI.Graphics;
+
+var gameArea = new PIXI.Graphics();
+gameArea.beginFill(0x000000);
+gameArea.drawRect(0, 0, renderer.view.width, renderer.view.height);
+gameArea.endFill();
+stage.addChild(gameArea);
+gameArea.interactive = true;
+gameArea.on('pointerdown', function(e) {
+	new Shape({
+		x: e.data.global.x,
+		y: e.data.global.y,
+		stage
+	});
+});
+
+var shape = new Shape({
+	x: 64,
+	y: 130,
+	stage
+});
+
+
+function update() {
+	renderer.render(stage);
+	requestAnimationFrame(update);
+}
+update();
