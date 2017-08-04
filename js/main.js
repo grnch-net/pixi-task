967890a2562df1248d033363d27d67f5aca79672
grnch <grnch.net@gmail.com>
2017-08-04 07:59:11 +0300

    Set scene parent element

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var type = (PIXI.utils.isWebGLSupported())? "WebGL" : "canvas";
 PIXI.utils.sayHello(type)
 
 var renderer = PIXI.autoDetectRenderer();
-document.body.appendChild(renderer.view);
+document.getElementById('work-area').appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 var Graphics = PIXI.Graphics;
